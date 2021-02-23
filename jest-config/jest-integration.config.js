e8404256096cb17ac3ad07842f7aaf153c8979d1
Gabriel-JM <gabriel.josemelo@hotmail.com>
2021-02-23 14:19:05 -0300

    Adding jest configuration files

---
+++ jest-config/jest-integration.config.js
@@ -0,0 +1,4 @@
+const jestConfig = require('./base-config')
+jestConfig.testMatch = ['**/*.test.ts']
+
+module.exports = jestConfig
---
+++ jest-config/jest-unit.config.js
@@ -0,0 +1,4 @@
+const jestConfig = require('./base-config')
+jestConfig.testMatch = ['**/*.spec.ts']
+
+module.exports = jestConfig
