3176d43c4d5cd0d6f1db59139c0265379386fa62
Gabriel-JM <gabriel.josemelo@hotmail.com>
2021-02-24 00:18:14 -0300

    Reorganize test suits

--- src/controllers/url/url-controller.spec.ts
+++ src/controllers/url/url-controller.spec.ts
@@ -20,59 +20,61 @@ function makeSut() {
 }
 
 describe('URL Controller', () => {
-  it('should return a 400 response if an invalid url is provided', async () => {
-    const { sut, urlValidatorSpy } = makeSut()
-
-    urlValidatorSpy.isValid.mockReturnValueOnce(false)
-
-    const request = {
-      params: {},
-      body: {
-        url: 'invalid.url.com'
+  describe('Create', () => {
+    it('should return a 400 response if an invalid url is provided', async () => {
+      const { sut, urlValidatorSpy } = makeSut()
+  
+      urlValidatorSpy.isValid.mockReturnValueOnce(false)
+  
+      const request = {
+        params: {},
+        body: {
+          url: 'invalid.url.com'
+        }
       }
-    }
-
-    const response = await sut.create(request)
-
-    expect(response.status).toBe(400)
-    expect(response.body).toEqual({
-      field: 'url',
-      error: 'Invalid URL format'
+  
+      const response = await sut.create(request)
+  
+      expect(response.status).toBe(400)
+      expect(response.body).toEqual({
+        field: 'url',
+        error: 'Invalid URL format'
+      })
     })
-  })
-
-  it('should return a 200 response if a valid url is provided', async () => {
-    const { sut, repositorySpy } = makeSut()
-    const urlValue = { url: 'shortened.url.com' }
-
-    repositorySpy.save.mockResolvedValueOnce(urlValue)
-
-    const response = await sut.create({
-      params: {},
-      body: { url: 'full.url.com' }
+  
+    it('should return a 200 response if a valid url is provided', async () => {
+      const { sut, repositorySpy } = makeSut()
+      const urlValue = { url: 'shortened.url.com' }
+  
+      repositorySpy.save.mockResolvedValueOnce(urlValue)
+  
+      const response = await sut.create({
+        params: {},
+        body: { url: 'full.url.com' }
+      })
+  
+      expect(response.status).toBe(200)
+      expect(response.body).toEqual(urlValue)
     })
-
-    expect(response.status).toBe(200)
-    expect(response.body).toEqual(urlValue)
-  })
-
-  it('should return a 500 response if repository throws some error', async () => {
-    const { sut, repositorySpy } = makeSut()
-    const errorMessage = 'unique constraint error'
-
-    repositorySpy.save.mockImplementationOnce(
-      () => { throw new Error(errorMessage) }
-    )
-
-    const response = await sut.create({
-      params: {},
-      body: { url: 'any.url.com' }
-    })
-
-    expect(response.status).toBe(500)
-    expect(response.body).toEqual({
-      field: '',
-      error: errorMessage
+  
+    it('should return a 500 response if repository throws some error', async () => {
+      const { sut, repositorySpy } = makeSut()
+      const errorMessage = 'unique constraint error'
+  
+      repositorySpy.save.mockImplementationOnce(
+        () => { throw new Error(errorMessage) }
+      )
+  
+      const response = await sut.create({
+        params: {},
+        body: { url: 'any.url.com' }
+      })
+  
+      expect(response.status).toBe(500)
+      expect(response.body).toEqual({
+        field: '',
+        error: errorMessage
+      })
     })
   })
 })
