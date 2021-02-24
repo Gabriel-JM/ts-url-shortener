6c2bfcd0f5f227f9734d7cdd85d9886b808c0598
Gabriel-JM <gabriel.josemelo@hotmail.com>
2021-02-24 18:43:27 -0300

    Added show method to UrlController to return a specific url

--- src/controllers/url/url-controller.ts
+++ src/controllers/url/url-controller.ts
@@ -2,6 +2,7 @@ import { IUrlRepository } from '../../protocols/domain'
 import { HttpRequest } from '../../protocols/infra'
 import { ShortenedUrl } from '../../protocols/models'
 import { IHashGenerator, IUrlValidator } from '../../protocols/utils'
+import { IExpirationValidator } from '../../protocols/utils/expiration-validator'
 import { fifteenDaysInMilliseconds } from '../../resources/constants'
 import { HttpResponse } from '../../resources/http/http-response'
 
@@ -9,8 +10,36 @@ export class UrlController {
   constructor(
     private readonly repository: IUrlRepository,
     private readonly urlValidator: IUrlValidator,
+    private readonly expirationValidator: IExpirationValidator,
     private readonly hashGenerator: IHashGenerator
   ) {}
+
+  async show(request: HttpRequest) {
+    try {
+      const { hash } = request.params
+
+      const shortenedUrl = await this.repository.findByHash(hash)
+
+      const isValid = this.expirationValidator.validate(
+        new Date(),
+        shortenedUrl?.expirationDate || ''
+      )
+
+      if(!isValid || !shortenedUrl) {
+        return HttpResponse.notFound({
+          field: 'url',
+          error: 'URL not found or expired'
+        })
+      }
+
+      return HttpResponse.ok({ url: shortenedUrl?.url })
+    } catch(err) {
+      return HttpResponse.serverError({
+        field: '',
+        error: err.message
+      })
+    }
+  }
 
   async create(request: HttpRequest) {
     try {
