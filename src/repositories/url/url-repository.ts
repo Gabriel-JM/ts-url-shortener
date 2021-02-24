012e442e41583ffc7c388b97acd4794b283d74b9
Gabriel-JM <gabriel.josemelo@hotmail.com>
2021-02-24 18:43:51 -0300

    Created Url Repository

---
+++ src/repositories/url/url-repository.ts
@@ -0,0 +1,22 @@
+import Knex from 'knex'
+import { IUrlRepository } from '../../protocols/domain'
+import { IncomingUrl } from '../../protocols/models'
+
+export class UrlRepository implements IUrlRepository {
+  constructor(
+    private readonly knex: Knex<IncomingUrl>
+  ) {}
+
+  async findByHash(hash: string) {
+    const [urlRegistry] = await this.knex('urls').where({ hash })
+
+    return urlRegistry || null
+  }
+
+  async save(urlRegistry: IncomingUrl) {
+    const [insertedId] = await this.knex('urls').insert(urlRegistry)
+    const [url] = await this.knex('urls').where('id', insertedId)
+
+    return url
+  }
+}
