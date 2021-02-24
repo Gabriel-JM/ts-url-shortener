552b483dee109d9a123617c78bc6267fbc838b81
Gabriel-JM <gabriel.josemelo@hotmail.com>
2021-02-24 18:38:27 -0300

    Change in url repository interface the method findByUrl to findByHash

--- src/protocols/domain/repository/url-repository.ts
+++ src/protocols/domain/repository/url-repository.ts
@@ -2,5 +2,5 @@ import { Repository } from '../../infra/repository/repository'
 import { IncomingUrl, ShortenedUrl } from '../../models'
 
 export interface IUrlRepository extends Repository<IncomingUrl, ShortenedUrl> {
-  findByUrl(url: string): Promise<ShortenedUrl | null>
+  findByHash(hash: string): Promise<IncomingUrl | null>
 }
