eae3d9c372c0583970daaa75e35246c59dd5ba7e
Gabriel-JM <gabriel.josemelo@hotmail.com>
2021-02-24 22:14:32 -0300

    Adding the swagger ui renderer

--- src/main/config/setup.ts
+++ src/main/config/setup.ts
@@ -1,5 +1,8 @@
 import express, { Application } from 'express'
+import swaggerUI from 'swagger-ui-express'
+import swaggerDocs from '../../../swagger.json'
 
 export default (app: Application) => {
   app.use(express.json())
+  app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs))
 }
