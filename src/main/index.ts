3e600ffda61ddfc05d10ed3c8e18e764c73014db
Gabriel-JM <gabriel.josemelo@hotmail.com>
2021-03-27 23:34:46 -0300

    Add some logic in server start function to retry database connection at least 5 times

--- src/main/index.ts
+++ src/main/index.ts
@@ -2,19 +2,25 @@ import 'dotenv-safe/config'
 import { connectDatabase } from '../resources/database'
 import { app } from './app'
 
-const knexDB = connectDatabase()
 const port = process.env.PORT || 3200
 
-knexDB
-  .migrate
-  .latest()
-  .then(() => {
-    app.listen(port, () => {
-      console.log(`Server Running at port ${port}`)
-    })
-  })
-  .catch(err => {
-    console.log('Initializing error...')
-    console.error(err)
-  })
-;
+async function start() {
+  let retries = 5
+  while(retries) {
+    try {
+      const knexDB = connectDatabase()
+      await knexDB.migrate.latest()
+
+      return app.listen(port, () => {
+        console.log(`Server Running at port ${port}`)
+      })
+    } catch(err) {
+      console.error(err)
+      retries--
+      console.log(`Retries left: ${retries}`)
+      await new Promise(res => setTimeout(res, 5000))
+    }  
+  }
+}
+
+start()
