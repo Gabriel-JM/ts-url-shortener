d998fadde091d49938f9359bc6c1e1351fe5d85f
Gabriel-JM <gabriel.josemelo@hotmail.com>
2021-04-04 18:08:47 -0300

    Adding cors module to configure CORS headers in the server

--- src/main/config/setup.ts
+++ src/main/config/setup.ts
@@ -1,8 +1,12 @@
 import express, { Application } from 'express'
+import cors from 'cors'
 import swaggerUI from 'swagger-ui-express'
 import swaggerDocs from '../../../swagger.json'
 
 export default (app: Application) => {
   app.use(express.json())
+  app.use(cors({
+    origin: 'http://localhost:3500'
+  }))
   app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs))
 }
