d3bdb3118f433be176a7140bed0aa8ed8b841699
Gabriel-JM <gabriel.josemelo@hotmail.com>
2021-04-15 22:07:28 -0300

    Change IUrlRepository interface to add findByUrl method

--- src/protocols/domain/repository/url-repository.ts
+++ src/protocols/domain/repository/url-repository.ts
@@ -3,4 +3,5 @@ import { IncomingUrl } from '../../models'
 
 export interface IUrlRepository extends Repository<IncomingUrl> {
   findByHash(hash: string): Promise<IncomingUrl | null>
+  findByUrl(url: string): Promise<IncomingUrl | null>
 }
