cdee380654b43ff307b8056bcb840a13dc5d925a
Gabriel-JM <gabriel.josemelo@hotmail.com>
2021-05-11 22:15:35 -0300

    build: change database

--- src/resources/database/index.ts
+++ src/resources/database/index.ts
@@ -3,8 +3,11 @@ import path from 'path'
 
 export function connectDatabase() {
   const dbConfig = <Knex.Config> {
-    client: 'postgresql',
-    connection: process.env.DATABASE_URL,
+    client: 'sqlite3',
+    connection: {
+      filename: path.join(__dirname, 'url-shortener.db'),
+      charset: 'utf8'
+    },
     migrations: {
       extension: 'ts',
       directory: path.join(__dirname, 'migrations')
